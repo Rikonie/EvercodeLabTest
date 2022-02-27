5917a8507ce22517001752e3e158cfa2f99c6390
Rikonie <Rikonie@bk.ru>
2022-02-27 12:31:51 +0300

    Чуть чуть подредактировала страницу Меню

--- src/menu.tsx
+++ src/menu.tsx
@@ -3,9 +3,9 @@ import {Link} from "react-router-dom";
 
 export const Menu = () =>{
     return (
-        <>
-            <nav><Link to ='/balance'>Проверить баланс кошелька</Link></nav>
-            <nav><Link to ='/crypto'>Посмотреть цены на криптовалюты</Link></nav>
+        <><div>Меню:</div>
+            <nav><Link to ='/balance'>- Проверить баланс кошелька</Link></nav>
+            <nav><Link to ='/crypto'>- Посмотреть цены на криптовалюты</Link></nav>
         </>
     )
 };
